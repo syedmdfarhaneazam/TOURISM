36d27cbab6b97f0b31163563beb29c67dbfcf13d
syedmdfarhaneazam <syedmdfarhaneazam@gmail.com>
2024-10-24 11:47:32 +0000

    Complete
    sorts the places by your location and displays it in the most optimal way

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  //useEffect take 2 funtion 1st your side effect code in a anonynous funtoin  , 2nd arg is an array of dependencies 
+  //excecutes afte the app component is exceuted
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPLaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);// navigator - > its is provided by the browser it returns a position object after it gets the location
+      setAvailablePlaces(sortedPLaces);
+    });
+  }, []); // if you define the dependencies then react will only excecute the useEffect when the dependency array is changed , here its empty its never gonna change so the sortting works ony once hence no infinit loop
+
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +43,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem("selectedPlaces", JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -35,6 +55,7 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+    localStorage.setItem("selectedPlace", JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)))
   }
 
   return (
@@ -63,7 +84,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by your distance ....."
           onSelectPlace={handleSelectPlace}
         />
       </main>
